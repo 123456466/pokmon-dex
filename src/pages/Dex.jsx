12b8bd3e7182129723dffcb91e5e3958bf4d2845
123456466 <sunji7378@gmail.com>
2025-02-03 15:37:25 +0900

    add : 리엑트 생성 및 라우팅 설정

---
+++ src/pages/Dex.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dex = () => {
+    return <div>DEX</div>
+}
+
+export default Dex
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate} from 'react-router-dom'
+
+const Home = () => {
+    const navigate = useNavigate()
+
+    return <div>
+        <h1>포켓몬스터</h1>
+        <button onClick={() => {
+            navigate('/Dex')
+        }}>시작하기</button>
+    </div>
+}
+
+export default Home
