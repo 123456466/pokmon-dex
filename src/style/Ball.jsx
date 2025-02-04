bcc20500ece93d9aeb9e6ec431ec12f44c62113b
123456466 <sunji7378@gmail.com>
2025-02-04 16:37:00 +0900

    style : 기본적인 정렬

---
+++ src/style/Ball.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Ball = styled.img`
+    width: 8%;
+    margin: 2%;
+`
+
+export default Ball
---
+++ src/style/BallsInBag.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const BallsInBag = styled.div`
+        display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+`
+
+export default BallsInBag
