f0d44d1aacfe454c5f878b693af67fe6fa8a0cd2
123456466 <sunji7378@gmail.com>
2025-02-11 14:55:42 +0900

    style : 타입에 따라 모달창 배경 변화

--- src/style/modal/DetailBackGruond.jsx
+++ src/style/modal/DetailBackGruond.jsx
@@ -3,11 +3,28 @@ import SetColor from "./SetColor";
 
 const DetailBackGround = styled.div`
   display: flex;
-  background-color: ${(props) => SetColor.find((e)=>{console.log(e.type === props.types)})};
+  background: linear-gradient(
+    315deg,
+    ${(props) =>
+      SetColor.find((e) => {
+        return e.type === String(...props.types);
+      }).color},
+    ${(props) => {
+      if (props.types.length === 2) {
+        return SetColor.find((e) => {
+          return e.type === String(...props.types[1]);
+        }).color;
+      } else {
+        return SetColor.find((e) => {
+          return e.type === String(...props.types);
+        }).color + '5e';
+      }
+    }}
+  );
   height: 100%;
   width: 100vw;
   align-items: center;
   justify-content: center;
 `;
 
-export default DetailBackGround
+export default DetailBackGround;
